4875a366e5a1402f127b229820b0bfb31a2a013e
SalahHammash <s.m.hammash@gmail.com>
2023-03-23 20:00:06 +0300

    i have done API 2 routes '

--- controllers/API-func.js
+++ controllers/API-func.js
@@ -1,1 +1,59 @@
 
+
+const axios = require("axios");
+
+const getAllExercises= (req, res) => {
+
+    try {
+        const options = {
+            method: 'GET',
+            url: 'https://exercisedb.p.rapidapi.com/exercises',
+            headers: {
+                'X-RapidAPI-Key': '46f6d7e551msh90f9b611d8c4048p12e502jsn99a94aea856c',
+                'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+            }
+        };
+        axios.request(options).then((response) => {
+            console.log(response.data);
+            const data = response.data
+            res.send(data)
+        })
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+
+
+const getExercisesByPart =  (req, res) => {
+    try {
+
+
+        const bodypart = req.params.bodyPart;
+
+        const options = {
+            method: 'GET',
+            url: `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodypart}`,
+            headers: {
+                'X-RapidAPI-Key': '1da0e2a338msh35e39fa6b141db7p18cf26jsnf57ff582e33e',
+                'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options)
+            .then((response) => {
+                const data = response.data
+                res.send(data)
+
+            })
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+module.exports = { getAllExercises, getExercisesByPart} 
--- server.js
+++ server.js
@@ -2,10 +2,14 @@
 const express = require("express");
 const cors = require("cors");
 const pg = require("pg");
+const { default: axios } = require("axios");
 require("dotenv").config();
 const server = express();
 const PORT = process.env.PORT || 5000;
 const client = new pg.Client(process.env.DATABASE_URL);
+const {getAllExercises , getExercisesByPart} = require("./controllers/API-func")
+
+
 
 server.use(cors());
 server.use(express.json());
@@ -13,6 +17,11 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.send("hello world");
 });
+
+server.get("/allExercises", getAllExercises);
+server.get("/allExercisesByBodypart/:bodypart", getExercisesByPart)
+
+
 
 client.connect().then(() => {
   server.listen(PORT, () => {
