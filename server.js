170adbe9c6633c55a351a0db964f07eac1bb6d76
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-03-23 22:37:31 +0300

    just some organization

--- server.js
+++ server.js
@@ -1,53 +1,45 @@
 "use strict";
-
 const express = require("express");
 const cors = require("cors");
-const server = express();
-
-server.use(cors());
-server.use(express.json());
-
 require("dotenv").config();
 
-
-const PORT = process.env.PORT || 5000;  // http://localhost:5000/
-
-
-
-const { client, getExerciseForOneUser } = require("./controllers/database-func");
+const {
+  client,
+  getExerciseForOneUser,
+} = require("./controllers/database-func");
 const { getExerciseListByEquipment } = require("./controllers/API-func");
-const { handleServerError , handlePageNotFoundError } = require('./controllers/handle-Error-func');
+const {
+  handleServerError,
+  handlePageNotFoundError,
+} = require("./controllers/handle-Error-func");
 
+const server = express();
+const PORT = process.env.PORT || 5000; // http://localhost:5000/
 
-
+server.use(express.json());
+server.use(cors());
 
 server.get("/", (req, res) => {
   res.send("hello world");
 });
 
-
 /////////////////// API routes //////////////////
 server.get("/exerciseByEquipment/:equipment", getExerciseListByEquipment);
-
 
 ////////////////// DATABASE routes ///////////////////
 server.get("/exerciseForOneUser/:email", getExerciseForOneUser);
 
-
-// Add error handling middleware to the server
+///////// error handling middleware to the server//////////////////
 server.use(handleServerError);
 server.use(handlePageNotFoundError);
 
-
-
-
-client.connect()
+client
+  .connect()
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Port ${PORT} is ready.`);
-    })
+    });
   })
   .catch((err) => {
     handleServerError(error, req, res);
-  })
-  
+  });
